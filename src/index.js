f889ab2c6dd99b94351ef8378b2477caa4f1dd82
deepsick <mav96@bk.ru>
2020-02-07 11:22:22 +0300

    Исправляет имя переменной

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
-const Settings = {
-  ERRORS_COUNT: 3
-};
+
+const ERRORS_COUNT = 3;
 
 ReactDOM.render(
     <App
-      errorsCount={Settings.ERRORS_COUNT}
+      errorsCount={ERRORS_COUNT}
     />,
     document.querySelector(`#root`)
 );
