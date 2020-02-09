8eeee5b8e9b9320e1ab4df4d4ed31a80d4867369
deepsick <mav96@bk.ru>
2020-02-09 20:49:53 +0300

    Убирает лишние строки пустые

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,6 @@
-
 import React from "react";
 import PropTypes from "prop-types";
-
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
-
 
 const App = (props) => {
   const {errorsCount} = props;
